c295d6b1d957f0bfd8a20003c3713d5a5a312d22
ChanKokLeong <ckleong3@singnet.com.sg>
2025-03-24 21:13:11 +0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,34 +1,29 @@
 Task-1
-# To create a function that would swap the value of x and y using only x and y as numeric. If one or both values are not numeric the function to return -1 and print the result that one or both values are not numeric.
-def swap(x, y): # define function to swap values x and y
-  if not (isinstance(x, (int, float) and y, (int, float))):  # check if both values x and y are numeric
-    return -1  # if one or both values are not numeric return -1
+# To create a function that would swap the value of x and y using only x and y as numeric. 
+# If one or both values are not numeric the function to return -1 and print the result that one or both values are not numeric.
 
-x, y = y, x
+def swap(x, y):
+    if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):  # check if both x and y are numeric
+        print("One or both values are not numeric.")
+        return -1
+    x, y = y, x  # Swap the values
+  
+    print(f"Swapped value: x = {x}, y = {y}")
+  
+    return x, y
 
-print(f'(Swapped value: x = {x}, y = {y}')
-return x, y
-
-result = swap(9, 21)  # example of both values are numeric 
-if result == -1:
-
-result = swap('Bus', 21)  # example of one value not numeric
-if result == -1:
-  print('One or both values are not numeric') 
 
 
 Task-2
-# Invoke the function swap using the following scenarios: 1-('Apple', 10) and 2-(9, 17)
-def swap(x, y)
-  if not(isinstance(x, (int, float) and y, (int, float))):
-    return -1
-
-x, y = y, x
+# Invoke the function swap using the following scenarious:
+# ('Apple', 10)
+# (9, 17)
 
 result = swap('Apple', 10)
 if result == -1:
-  print('One or both values are not numeric')
+    print('Swap not possible')
+
 
 result = swap(9, 17)
-if result == -1:
-  print('One or both values are not numeric')
+if result != -1:
+    print('Swap possible')
